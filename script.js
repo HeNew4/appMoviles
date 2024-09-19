b7e1db1656d54be0d9af4b1ba57a2b31244fc65a
HeNew4 <angelmaclovio@yahoo.com>
2024-09-18 20:54:04 -0600

    [Añadido] 18-09-2024 Maclovio Morales Angel
    El documento de JS fue añadido y se puso hizo una mini explicación de
    este leguaje...
    
    Hola Roman :DDDDDDDD

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('Hola Mundo');
+
+// 18-08-2024
+alert('Hola Mundo')
+
+const nombre = "Angel";
+let edad = 14;
+var edad2 = 123;
+
+/* TIPOS DE DATOS */
+/* 
+string
+number
+float
+bigInt
+boolean
+char
+NaN
+Infinity
+Hexadecimal
+Objects
+*/
+
+/* '10' + 1 = 101 */
+/* '10' - 1 = 9 */
+
+let a = 5;
+let b = 2;
+
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let divicion = a/b;
