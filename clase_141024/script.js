7b3d0903f69c115f6cf93494d92253ea406aee0f
HeNew4 <angelmaclovio@yahoo.com>
2024-10-14 20:03:36 -0600

    OK

---
+++ clase_141024/script.js
@@ -0,0 +1,62 @@
+// /* =========== FUNCIONES EN JS =========== */
+
+
+// function hello() {
+//     console.log('Hola JS');
+//     console.log('Hola HTML');
+//     console.log('Hola CSS');
+// }
+
+
+// hello()
+
+// /**
+//  * Funcion 🐄
+//  * @returns retorna una cadena que dice `Hola JS`
+//  */
+// function hello() {
+//     return 'Hola JS';
+// }
+
+// // const result = hello();
+// console.log(hello());
+
+// function hello() {
+//     return {
+//         nombre: 'Roul'
+//     }
+// }
+
+// console.log(hello());
+
+// function hello() {
+//     return function() {
+//         return 'Hola JS';
+//     }
+// }
+
+// console.log(hello()());
+
+// /**
+//  * 
+//  * @param {string} name Nombre tuyo
+//  * @returns cadena saludando tu nombre
+//  */
+// function hello(name) {
+//     return 'Hola ' + name;
+// }
+
+
+// console.log(hello('Angel Maclovio 🐄'));
+
+/**
+ * Operación con dos numero
+ * @param {number} x primer numero
+ * @param {number} y segundo numero
+ * @returns returna el resultado
+ */
+function operacion(x, y) {
+    return x - y;
+}
+
+console.log(operacion(30, 10));
