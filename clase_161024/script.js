a2c8d04f15424bce15b95b94e404b5d2f5fa5f84
HeNew4 <angelmaclovio@yahoo.com>
2024-10-16 20:41:14 -0600

    [Add] Se añadio ejemplo de manipulación del DOM

---
+++ clase_161024/script.js
@@ -0,0 +1,11 @@
+// Variable donde creamos en heading
+const heading = document.createElement('h1');
+
+// Contenido del Heading
+heading.textContent = 'MuuUuuU 🐄';
+
+// Imprimimos el contenido del Heading
+console.log(heading.textContent);
+
+// Se añade al body un nuevo hijo
+document.body.appendChild(heading)
