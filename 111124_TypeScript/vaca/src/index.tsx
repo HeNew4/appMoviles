927e26ebc4d159f8b9f3425b1eeb929926d202c0
HeNew4 <angelmaclovio@yahoo.com>
2024-11-11 20:18:11 -0600

    [Añado] React con Farm js

---
+++ 111124_TypeScript/vaca/src/index.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client';
+import { Main } from './main';
+import './index.css'
+
+
+const container = document.querySelector('#root');
+
+if (container) {
+    
+    const root = createRoot(container);
+    
+    root.render(<Main />);
+
+}
+
+
